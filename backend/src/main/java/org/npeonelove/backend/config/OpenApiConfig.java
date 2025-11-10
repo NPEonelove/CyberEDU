866e500831f4472de8680575b9e19ae50e70769f
NPEonelove <124193334+NPEonelove@users.noreply.github.com>
2025-11-10 18:40:57 +0400

    Начал создание auth

---
+++ backend/src/main/java/org/npeonelove/backend/config/OpenApiConfig.java
@@ -0,0 +1,19 @@
+package org.npeonelove.backend.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+
+@OpenAPIDefinition(
+        info = @Info(
+                title = "CyberEDU API",
+                version = "1.0.0",
+                contact = @Contact(
+                        name = "Ivan Kochetov",
+                        url = "https://t.me/NPEonelove"
+                )
+        )
+)
+public class OpenApiConfig {
+
+}
