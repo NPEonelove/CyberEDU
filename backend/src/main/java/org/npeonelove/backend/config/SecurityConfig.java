18a5381c0d73d8b8faf570570f28c4ee08b509d1
NPEonelove <124193334+NPEonelove@users.noreply.github.com>
2025-11-14 11:48:38 +0400

    Update SecurityConfig.java

--- backend/src/main/java/org/npeonelove/backend/config/SecurityConfig.java
+++ backend/src/main/java/org/npeonelove/backend/config/SecurityConfig.java
@@ -23,7 +23,6 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .cors(AbstractHttpConfigurer::disable)
                 .httpBasic(AbstractHttpConfigurer::disable)
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(authorizeRequests -> authorizeRequests
